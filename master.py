1d9524923761a8a621951d495e322b4d23078cf0
ricardo <hakdigital98@gmail.com>
2022-04-23 21:01:56 -0500

    home

--- master.py
+++ master.py
@@ -1,1 +1,14 @@
-print("hello world")
+#FAstapi
+from fastapi import FastAPI,status
+
+
+app=FastAPI()
+
+
+@app.get(
+   path= "/",
+   status_code=status.HTTP_200_OK,
+   tags=["home"]
+    )
+def home():
+    return{"hello:world"}
